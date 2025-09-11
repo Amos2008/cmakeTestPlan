fa25bb25188039cdf8a2ad193a26b1283d2076d5
unknown <lvhfei@gmail.com>
2025-09-11 18:05:24 +0800

    thirdParty

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,7 +1,14 @@
 #include    "process.h"
+#include<Eigen/Dense>
+using Eigen::MatrixXd;
 void Process::planProcess()
 {
     std::cout << "This is Process class" << std::endl;
     my_map.map_info();
+
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    std::cout<<"m:"<<m<<std::endl;
+
     std::cout << "Process plan completed!" << std::endl;
 };
